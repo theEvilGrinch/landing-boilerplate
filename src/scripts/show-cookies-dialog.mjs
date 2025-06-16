7ad8857fa9b8869213fd64242252b89f2a4baf13
Andrew Dmitriev <andrew.dmitriev.22@gmail.com>
2025-06-16 15:06:53 +0300

    fix: update cookie dialog implementation
    - Refactor cookie consent logic in show-cookies-dialog.mjs
    - Update script loading in index.html

--- src/scripts/show-cookies-dialog.mjs
+++ src/scripts/show-cookies-dialog.mjs
@@ -1,18 +1,15 @@
 function setCookies() {
-  window.addEventListener('DOMContentLoaded', () => {
-    const dialog = document.getElementById('cookieDialog');
-    const button = dialog.querySelector('.js-CookieCloseBtn');
+  const dialog = document.getElementById('cookieDialog');
+  const button = dialog.querySelector('.js-CookieCloseBtn');
 
-    if (!document.cookie.includes('cookieConsent=accepted')) {
-      dialog.show();
-    }
+  if (!document.cookie.includes('cookieConsent=accepted')) {
+    dialog.show();
+  }
 
-    button.addEventListener('click', () => {
-      document.cookie = 'cookieConsent=accepted; Max-Age=31536000; Path=/';
-      dialog.close();
-    });
+  button.addEventListener('click', () => {
+    document.cookie = 'cookieConsent=accepted; Max-Age=31536000; Path=/';
+    dialog.close();
   });
-
 }
 
 export default setCookies;
